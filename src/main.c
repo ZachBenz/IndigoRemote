fc28fe21cb56c844a569705e352c0cfb7cf69979
ZachBenz <zbenz@me.com>
2014-02-04 22:07:57 -0800

    Latest updates

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 #define NUM_MENU_SECTIONS 1
 #define NUM_MENU_ICONS 3
-#define NUM_FIRST_MENU_ITEMS 3
+#define NUM_FIRST_MENU_ITEMS 2
 
 static Window *window;
 
@@ -14,9 +14,6 @@ static MenuLayer *menu_layer;
 static GBitmap *menu_icons[NUM_MENU_ICONS];
 
 static int current_icon = 0;
-
-// You can draw arbitrary things in a menu item such as a background
-static GBitmap *menu_background;
 
 // A callback is used to specify the amount of sections of menu items
 // With this, you can dynamically add and remove sections
@@ -39,22 +36,13 @@ static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t secti
 // A callback is used to specify the height of the section header
 static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
   // This is a define provided in pebble.h that you may use for the default height
-  return MENU_CELL_BASIC_HEADER_HEIGHT;
+  // return MENU_CELL_BASIC_HEADER_HEIGHT;
+  return 0;
 }
 
 // Here we draw what each header is
 static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
-  // Determine which section we're working with
-  switch (section_index) {
-    case 0:
-      // Draw title text in the section header
-      menu_cell_basic_header_draw(ctx, cell_layer, "Some example items");
-      break;
-
-    case 1:
-      menu_cell_basic_header_draw(ctx, cell_layer, "One more");
-      break;
-  }
+  // Nothing to do - we're not using headers
 }
 
 // This is the menu item draw callback where you specify what each item should look like
@@ -66,30 +54,15 @@ static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuI
       switch (cell_index->row) {
         case 0:
           // This is a basic menu item with a title and subtitle
-          menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
+          menu_cell_basic_draw(ctx, cell_layer, "Devices", "Control devices", menu_icons[current_icon]);
           break;
 
         case 1:
           // This is a basic menu icon with a cycling icon
-          menu_cell_basic_draw(ctx, cell_layer, "Icon Item", "Select to cycle", menu_icons[current_icon]);
-          break;
-
-        case 2:
-          // Here we use the graphics context to draw something different
-          // In this case, we show a strip of a watchface's background
-          graphics_draw_bitmap_in_rect(ctx, menu_background,
-              (GRect){ .origin = GPointZero, .size = layer_get_frame((Layer*) cell_layer).size });
+          menu_cell_basic_draw(ctx, cell_layer, "Actions", "Execute actions", menu_icons[(current_icon + 1) % NUM_MENU_ICONS]);
           break;
       }
       break;
-
-    case 1:
-      switch (cell_index->row) {
-        case 0:
-          // There is title draw for something more simple than a basic menu item
-          menu_cell_title_draw(ctx, cell_layer, "Final Item");
-          break;
-      }
   }
 }
 
@@ -98,6 +71,7 @@ void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *da
   // Use the row to specify which item will receive the select action
   switch (cell_index->row) {
     // This is the menu item with the cycling icon
+	case 0:
     case 1:
       // Cycle the icon
       current_icon = (current_icon + 1) % NUM_MENU_ICONS;
@@ -116,9 +90,6 @@ void window_load(Window *window) {
   menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_BIG_WATCH);
   menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_SECTOR_WATCH);
   menu_icons[num_menu_icons++] = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON_BINARY_WATCH);
-
-  // And also load the background
-  menu_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_BRAINS);
 
   // Now we prepare to initialize the menu layer
   // We need the bounds to specify the menu layer's viewport size
@@ -154,9 +125,6 @@ void window_unload(Window *window) {
   for (int i = 0; i < NUM_MENU_ICONS; i++) {
     gbitmap_destroy(menu_icons[i]);
   }
-
-  // And cleanup the background
-  gbitmap_destroy(menu_background);
 }
 
 int main(void) {
