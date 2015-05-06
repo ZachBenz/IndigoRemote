9307e76e5c58fe1603f1a56e83f444cc71f4ed2c
Seth Goldman <seth@alum.mit.edu>
2015-05-06 13:12:32 -0700

    More refactoring and debugging of calls using digest authentication. Works in emulator but not on device

--- src/js-pre-browserify/pebble-js-app-pre-browserify.js
+++ src/js-pre-browserify/pebble-js-app-pre-browserify.js
@@ -64,7 +64,7 @@ var config_html; // see bottom of file
 var config = {
     reflectorAddress: "",
     serverAddress: "",
-    serverPort: 8176,
+    serverPort: "8176",
     userName: "",
     userPass: ""
 };
@@ -87,27 +87,32 @@ function init_config() {
 
     localVal = localStorage.getItem("userPass");
     if (localVal) config.userPass = localVal;
-
-    //    authenticateToServer();
 }
 
-function getHttpRequest(route, async, req) {
-    var authHdr = req !== null ? buildAuthResponse(req.getResponseHeader('WWW-Authenticate'), route) : "";
-    var url = "http://";
+function buildURL(route) {
+    var url = "";
     if (config.reflectorAddress !== null && config.reflectorAddress.length) {
-	url += config.reflectorAddress;
+	url += "https://" + config.reflectorAddress;
     } else {
-	url += config.serverAddress;
+	url += "http://" + config.serverAddress;
 	if (config.serverPort !== null && config.serverPort.length) 
 	    url += ":" + config.serverPort;
     }
     url += route;
+    return url;
+}
+
+function getHttpRequest(route, async, req) {
+    var url = buildURL(route);
     console.log("getHttpRequest: " + url);
+
+    var authHdr = buildAuthResponse(req, route);
     req = new XMLHttpRequest();
-    req.open('GET', url, async);
-    
+    req.open('GET', url, authHdr.length === 0 && async); // Second call is synchronous
+    req.setRequestHeader('Content-Type', 'application/json');
+
     if (authHdr.length) {
-	//	console.log("Setting Authorization: " + authHdr);
+	//console.log("Setting Authorization: " + authHdr);
 	req.setRequestHeader("Authorization", authHdr);
     }
     return req;
@@ -123,14 +128,19 @@ function makeHttpCallInternal(route, async, handler, origReq)
     var req = getHttpRequest(route, async, origReq);
     if (handler !== null) 
 	req.onload = function(e) {
-	    if (origReq === null && req.status == 401) {
+	    if (origReq === null && responseUnauthorized(req)) {
+		console.log("Making second call (asynchronous)");
 		return makeHttpCallInternal(route, async, handler, req);
 	    }
 	    handler(req, e);
 	};
-    req.send(null);
-    if (! async && handler === null && origReq === null && req.status == 401) {
+    req.send();
+    if (! async && handler === null && origReq === null && responseUnauthorized(req)) {
+	console.log("Making second call (synchronous)");
 	return makeHttpCallInternal(route, async, handler, req);
+    }
+    if (! responseOK(req)) {
+	console.log("Error: " + JSON.stringify(req));
     }
     return req;
 }
@@ -139,8 +149,7 @@ init_config();
 
 // Set callback for the app ready event
 Pebble.addEventListener("ready", function(e) {
-	console.log("Ready: " + e.ready);
-	console.log(e.type);
+	console.log("Ready to go: " + JSON.stringify(e));
     });
 
 Pebble.addEventListener("showConfiguration", function() {
@@ -150,11 +159,16 @@ Pebble.addEventListener("showConfiguration", function() {
     });
 
 Pebble.addEventListener("webviewclosed", function(e) {
-	send({"loading": 1});
 	console.log("configuration closed");
 	// webview closed
+	if (e.response === null || e.response.length === 0) {
+	    console.log("Changes aborted");
+	    return;
+	}
 	var options = JSON.parse(decodeURIComponent(e.response));
 	console.log("Options = " + JSON.stringify(options));
+	localStorage.setItem("reflectorAddress", options.reflectorAddress);
+	config.reflectorAddress = options.reflectorAddress;
 	localStorage.setItem("serverAddress", options.serverAddress);
 	config.serverAddress = options.serverAddress;
 	localStorage.setItem("serverPort", options.serverPort);
@@ -163,9 +177,7 @@ Pebble.addEventListener("webviewclosed", function(e) {
 	config.userName = options.userName;
 	localStorage.setItem("userPass", options.userPass);
 	config.userPass = options.userPass;
-	init_url_prefix();
-	getDevices();
-	getActions();
+	send({"loading": 1});
     });
 
 var messageQueue = [], queueInProgress = false, timeoutBackOff = DEFAULT_TIMEOUT_BACKOFF;
@@ -173,9 +185,8 @@ function sendNextInQueue() {
     if (messageQueue.length === 0) {
         queueInProgress = false;
         return;
-    } else {
-        queueInProgress = true;
     }
+    queueInProgress = true;
     var message = messageQueue[0];
     Pebble.sendAppMessage(message,
 			  function (e) {
@@ -227,15 +238,27 @@ function pad(num, size)
     return s;
 }
 
-function buildAuthResponse(challenge, uri) 
+function genNonce(len) {
+    var text = "";
+    var possible = "ABCDEF0123456789";
+    for(var i=0; i<len; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+function buildAuthResponse(req, uri) 
 {
-    if (challenge === null) return "";
+    if (req === null) return "";
+    var challenge = req.getResponseHeader('WWW-Authenticate');
+    if (challenge === null || challenge === undefined) return "";
     var pos = challenge.indexOf(" ");
-    var tokens = {cnonce: 'bd5fd9de93dccaa1'};
+    var tokens = {cnonce: genNonce(16)};
     var pairs = challenge.substr(pos).trim().split(',');
     tokens.nc = pad(nc++, 8);
 
     for (var token in pairs) {
+	//console.log(pairs[token].trim());
 	var pair = pairs[token].trim().split('=');
 	tokens[pair[0]] = pair[1];
     }
@@ -263,71 +286,85 @@ function buildAuthResponse(challenge, uri)
     return responseContentHeader;
 }
 
-function authenticateToServer() {
-    makeHttpCall("/", false, 
-		 function(req, e) {
-		     console.log("Request status is: " + req.status.toString());
-		 });
+function responseOK(req) {
+    return req.readyState == 4 && req.status == 200;
+}
+
+function responseUnauthorized(req) {
+    return req.readyState == 4 && req.status == 401;
+}
+
+/*
+192.168.10.28 - - [05/May/2015:19:44:43] "GET /devices.json HTTP/1.1" 401 330 "" "PebbleApp/20150219013617 CFNetwork/711.3.18 Darwin/14.0.0"
+192.168.10.28 - - [05/May/2015:19:44:43] "GET /devices.json HTTP/1.1" 401 330 "" "PebbleApp/20150219013617 CFNetwork/711.3.18 Darwin/14.0.0"
+192.168.10.28 - seth [05/May/2015:19:44:43] "GET /devices/index.json HTTP/1.1" 200 5833 "" "PebbleApp/20150219013617 CFNetwork/711.3.18 Darwin/14.0.0"
+
+192.168.10.102 - - [05/May/2015:20:08:30] "GET /devices.json HTTP/1.1" 401 330 "" "python-requests/2.5.0 CPython/2.7.6 Darwin/14.3.0"
+192.168.10.102 - seth [05/May/2015:20:08:30] "GET /devices/index.json HTTP/1.1" 200 5833 "" "python-requests/2.5.0 CPython/2.7.6 Darwin/14.3.0"
+
+192.168.10.102 - - [05/May/2015:20:04:46] "GET /devices/Seth Office.json HTTP/1.1" 401 330 "" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.45 Safari/537.36"
+192.168.10.102 - seth [05/May/2015:20:04:53] "GET /devices/Seth Office.json HTTP/1.1" 200 822 "" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.45 Safari/537.36"
+ */
+
+function buildDevice(data, isOn, isValid) {
+    return {"device_name": data.name.substring(0, MAX_DEVICE_NAME_LENGTH),
+	    "device_rest_url": data.restURL,
+	    "device_on": isOn,
+	    "verified": isValid
+	    };
+}
+
+function getDeviceDetails(response) {
+    devices = [];
+    deviceCount = 0;
+    
+    for (i=0; i<response.length; i++) {
+	var req = makeHttpCall(response[i].restURL, false, null);
+	if (responseOK(req)) {
+	    var deviceInfo = JSON.parse(req.responseText);
+	    devices[deviceCount++] = buildDevice(response[i], deviceInfo.isOn, true);
+	} else {
+	    // For some reason we're getting errors on these calls instead of having them authenticated.
+	    // The server reports back the correct information (401) but it's not received by Pebble.
+	    // It has to do with when the calls are synchronous as opposed to asynchronous. Synchronous
+	    // calls aren't working.
+	    console.log("Device Request: " + response[i].name + " returned error code " + req.statusText);
+	    devices[deviceCount++] = buildDevice(response[i], false, false);
+	}
+    }
+    
+    localStorage.setItem("deviceCount", deviceCount);
+    localStorage.setItem("devices", JSON.stringify(devices));
+
+    // We've got the total count of controllable devices, so send it out
+    sendDeviceCount(deviceCount);
+                
+    for (i=0; i<deviceCount; i++)
+	sendDeviceInfo(i, devices[i]);
 }
 
 function getDevices() {
     var uri = "/devices.json";
     var handler = function(req, e) {
-        if (req.status == 200) {
+        if (responseOK(req)) {
             var response = JSON.parse(req.responseText);
-            if (response.length > 0) {
-                // Pare down to just devices that have typeSupportsOnOff: true
-                deviceCount = 0;
-                devices = [];
-                
-                var i, j;
-                for (i = 0, j = response.length; i < j; i += 1) {
-		    var deviceRequest = makeHttpCall(response[i].restURL, false, null);
-                    if (deviceRequest.status == 200) {
-                        // Track device information
-                        var deviceInfo = JSON.parse(deviceRequest.responseText);
-                        if (deviceInfo.typeSupportsOnOff) {
-                            devices[deviceCount] =
-				{
-				    "device_name": deviceInfo.name.substring(0, MAX_DEVICE_NAME_LENGTH),
-				    "device_rest_url": response[i].restURL,
-				    "device_on": deviceInfo.isOn
-				};
-                            deviceCount++;
-                        }
-                    }
-                    else {
-                        console.log("Request for device " + response[i].restURL + " returned error code " + deviceRequest.status.toString());
-                    }
-                }
-                localStorage.setItem("deviceCount", deviceCount);
-                localStorage.setItem("devices", JSON.stringify(devices));
-                
-                // We've got the total count of controllable devices, so send it out
-                sendDeviceCount(deviceCount);
-                
-                // Now send out the information for each device
-                for (i = 0, j = devices.length; i < j; i += 1) {
-                    sendDeviceInfo(i, devices[i]);
-                }
-            }
-            else {
+            if (response.length > 0)
+		getDeviceDetails(response);
+            else
                 sendDeviceCount(0);
-            }
-        }  else {
-            console.log("Request returned error code " + req.status.toString());
-            sendDeviceCount(0);
-        }
+	} else {
+	    console.log("Request returned error code " + req.status.toString());
+	    sendDeviceCount(0);
+	}
     };
     makeHttpCall(uri, true, handler);
 }
 
 function toggleDeviceOnOff(deviceNumber) {
     var path = devices[deviceNumber].device_rest_url + "?toggle=1&_method=put";
+
     var handler = function(req, e) {
-        if (req.readyState == 4) {
-            // 200 - HTTP OK
-            if(req.status == 200) {
+            if (responseOK(req)) {
                 var deviceInfo = JSON.parse(req.responseText);
                 devices[deviceNumber].device_on = deviceInfo.isOn;
                 sendDeviceInfo(deviceNumber, devices[deviceNumber]);
@@ -335,8 +372,9 @@ function toggleDeviceOnOff(deviceNumber) {
             } else {
                 // TODO: inform pebble that toggle failed
                 console.log("Request returned error code " + req.status.toString());
+		console.log(JSON.stringify(e));
+		console.log(JSON.stringify(req));
             }
-        }
     };
     makeHttpCall(path, true, handler);
 }
@@ -355,21 +393,20 @@ function sendActionInfo(actionNumber, actionInfo) {
 function getActions() {
     var uri = "/actions.json";
     var handler = function(req, e) {
-        if(req.status == 200) {
+        if (responseOK(req)) {
             var response = JSON.parse(req.responseText);
             if (response.length > 0) {
                 actionCount = 0;
                 actions = [];
                 
-                var i,j;
-                for (i = 0, j = response.length; i < j; i += 1) {
+                var i;
+                for (i=0; i<response.length; i++) {
                     // Track action information
-                    actions[actionCount] =
+                    actions[actionCount++] =
 			{
 			    "action_name": response[i].name.substring(0, MAX_ACTION_NAME_LENGTH),
 			    "action_rest_url": response[i].restURL
 			};
-                    actionCount++;
                 }
                 localStorage.setItem("actionCount", actionCount);
                 localStorage.setItem("actions", JSON.stringify(actions));
@@ -378,9 +415,8 @@ function getActions() {
                 sendActionCount(actionCount);
                 
                 // Now send out the information for each action
-                for (i = 0, j = actions.length; i < j; i += 1) {
+                for (i=0; i<actions.length; i++)
                     sendActionInfo(i, actions[i]);
-                }
             }
             else {
                 sendActionCount(0);
@@ -395,16 +431,14 @@ function getActions() {
 
 function executeAction(actionNumber) {
     var path = actions[actionNumber].action_rest_url + "?_method=execute";
+
     var handler = function(req, e) {
-        if (req.readyState == 4) {
-            // 200 - HTTP OK
-            if (req.status == 200) {
+        if (responseOK(req)) {
                 sendActionInfo(actionNumber, actions[actionNumber]);
             } else {
                 // TODO: inform pebble that action failed
                 console.log("Request returned error code " + req.status.toString());
             }
-        }
     };
     makeHttpCall(path, true, handler);
 }
@@ -412,18 +446,15 @@ function executeAction(actionNumber) {
 function dimDevice(deviceNumber, dimLevel) {
     var path = devices[deviceNumber].device_rest_url + "?brightness=" + dimLevel + "&_method=put";
     var handler = function(req, e) {
-        if (req.readyState == 4) {
-            // 200 - HTTP OK
-            if(req.status == 200) {
-                var deviceInfo = JSON.parse(req.responseText);
-                devices[deviceNumber].device_on = deviceInfo.isOn;
-                sendDeviceInfo(deviceNumber, devices[deviceNumber]);
-                localStorage.setItem("devices", JSON.stringify(devices));
-            } else {
-                // TODO: inform pebble that dim failed
-                console.log("Request returned error code " + req.status.toString());
-            }
-        }
+	if (responseOK(req)) {
+	    var deviceInfo = JSON.parse(req.responseText);
+	    devices[deviceNumber].device_on = deviceInfo.isOn;
+	    sendDeviceInfo(deviceNumber, devices[deviceNumber]);
+	    localStorage.setItem("devices", JSON.stringify(devices));
+	} else {
+	    // TODO: inform pebble that dim failed
+	    console.log("Request returned error code " + req.status.toString());
+	}
     };
     makeHttpCall(path, true, handler);
 }
@@ -508,11 +539,11 @@ p,a {color: rgb(200,200,200)}\
 </form>\
 <script>\
 var config = JSON.parse(\'__CONFIG__\');\
+document.getElementById("reflector-address").value = config.reflectorAddress;\
 document.getElementById("server-address").value = config.serverAddress;\
 document.getElementById("server-port").value = config.serverPort;\
 document.getElementById("user-name").value = config.userName;\
 document.getElementById("user-pass").value = config.userPass;\
-document.getElementById("reflector-address").value = config.reflectorAddress;\
 function onSubmit(e) {\
 var result = {\
 reflectorAddress: document.getElementById("reflector-address").value,\
